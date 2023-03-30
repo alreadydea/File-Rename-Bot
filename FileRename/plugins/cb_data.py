68cc5eb0526e481feb0347d62ace4cce4df62c89
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-03-30 18:38:19 +0530

    Update cb_data.py

--- FileRename/plugins/cb_data.py
+++ FileRename/plugins/cb_data.py
@@ -3,7 +3,7 @@ from pyrogram import Client, filters
 from pyrogram.types import (  InlineKeyboardButton, InlineKeyboardMarkup,ForceReply)
 from hachoir.metadata import extractMetadata
 from hachoir.parser import createParser
-from FileRenamehelper.database import db
+from FileRename.helper.database import db
 import os 
 import humanize
 from PIL import Image
