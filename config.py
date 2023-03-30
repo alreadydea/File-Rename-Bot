4d98d919ab9e6ddcd67cdaad890c3e72e1977c9e
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-03-30 18:05:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,5 +9,4 @@ FORCE_SUB = os.environ.get("FORCE_SUB", "")
 DB_NAME = os.environ.get("DB_NAME","")     
 DB_URL = os.environ.get("DB_URL","")
 FLOOD = int(os.environ.get("FLOOD", "10"))
-START_PIC = os.environ.get("START_PIC", "")
 ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get("ADMIN", "").split()]
