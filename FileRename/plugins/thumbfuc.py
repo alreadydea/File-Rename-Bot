cf4986822c112f56444c64de76de3bcb122e3d2d
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-03-30 17:33:33 +0530

    Update thumbfuc.py

--- FileRename/plugins/thumbfuc.py
+++ FileRename/plugins/thumbfuc.py
@@ -1,5 +1,5 @@
 from pyrogram import Client, filters
-from helper.database import db
+from FileRename.helper.database import db
 
 @Client.on_message(filters.private & filters.command(['viewthumb']))
 async def viewthumb(client, message):    
@@ -9,16 +9,16 @@ async def viewthumb(client, message):
 	   chat_id=message.chat.id, 
 	   photo=thumb)
     else:
-        await message.reply_text("😔 __**𝚈𝙾𝚄 𝙳𝙾𝙽𝚃 𝙷𝙰𝚅𝙴 𝙰𝙽𝚈 𝚃𝙷𝚄𝙼𝙱𝙽𝙰𝙸𝙻**__") 
+        await message.reply_text("**ʏᴏᴜ ᴅᴏɴᴛ ʜᴀᴠᴇ ᴀɴʏ ᴛʜᴜᴍʙɴᴀɪʟ.**") 
 		
 @Client.on_message(filters.private & filters.command(['delthumb']))
 async def removethumb(client, message):
     await db.set_thumbnail(message.from_user.id, file_id=None)
-    await message.reply_text("❌️ __**𝚈𝙾𝚄𝚁 𝚃𝙷𝚄𝙼𝙱𝙽𝙰𝙸𝙻 𝚂𝚄𝙲𝙲𝙴𝚂𝚂𝙵𝚄𝙻𝙻𝚈 𝙳𝙴𝙻𝙴𝚃𝙴𝙳**__")
+    await message.reply_text("❌️ **ʏᴏᴜʀ ᴛʜᴜᴍʙɴᴀɪʟ sᴜᴄᴄᴇssғᴜʟʟʏ ᴅᴇʟᴇᴛᴇᴅ.**")
 	
 @Client.on_message(filters.private & filters.photo)
 async def addthumbs(client, message):
-    mkn = await message.reply_text("Please Wait ...")
+    mkn = await message.reply_text("ᴘʟᴇᴀsᴇ ᴡᴀɪᴛ...↻")
     await db.set_thumbnail(message.from_user.id, file_id=message.photo.file_id)                
-    await mkn.edit("✅️ __**𝚈𝙾𝚄𝚁 𝚃𝙷𝚄𝙼𝙱𝙽𝙰𝙸𝙻 𝚂𝚄𝙲𝙲𝙴𝚂𝚂𝚂𝙵𝚄𝙻𝙻𝚈 𝚂𝙰𝚅𝙴𝙳**__")
+    await mkn.edit("✅️**ʏᴏᴜʀ ᴛʜᴜᴍʙɴᴀɪʟ sᴜᴄᴄᴇssғᴜʟʟʏ sᴀᴠᴇᴅ**")
 	
