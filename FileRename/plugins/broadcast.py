0d13911888db3d1841e535a7486e9c1ca1c61396
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-09-11 12:52:57 +0530

    Add files via upload

--- FileRename/plugins/broadcast.py
+++ FileRename/plugins/broadcast.py
@@ -1,106 +1,64 @@
-import logging
+import os
 import time
-import asyncio
+import asyncio 
+import logging 
 import datetime
-from Hiroko import Hiroko
-from config import OWNER_ID
-from Hiroko.Helper.database.chatsdb import *
-from Hiroko.Helper.database.usersdb import *
+from config import ADMIN
+from FileRename.helper.database import db
+from pyrogram.types import Message
 from pyrogram import Client, filters
-from pyrogram.errors import InputUserDeactivated, UserNotParticipant, FloodWait, UserIsBlocked, PeerIdInvalid, ChatAdminRequired
+from pyrogram.errors import FloodWait, InputUserDeactivated, UserIsBlocked, PeerIdInvalid
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+ 
+@Client.on_message(filters.command("stats") & filters.user(ADMIN))
+async def get_stats(bot :Client, message: Message):
+    mr = await message.reply('**ᴀᴄᴄᴇssɪɴɢ ᴅᴇᴛᴀɪʟs....**')
+    total_users = await db.total_users_count()
+    await mr.edit( text=f"⊚ ᴛᴏᴛᴀʟ ᴜsᴇʀs |`{total_users}`")
 
-# Broadcast messages to individual users
-
-async def broadcast_messages(user_id, message):
+@Client.on_message(filters.command("broadcast") & filters.user(ADMIN) & filters.reply)
+async def broadcast_handler(bot: Client, m: Message):
+    all_users = await db.get_all_users()
+    broadcast_msg = m.reply_to_message
+    sts_msg = await m.reply_text("ʙʀᴏᴀᴅᴄᴀsᴛ sᴛᴀʀᴛᴇᴅ !!") 
+    done = 0
+    failed = 0
+    success = 0
+    start_time = time.time()
+    total_users = await db.total_users_count()
+    async for user in all_users:
+        sts = await send_msg(user['_id'], broadcast_msg)
+        if sts == 200:
+           success += 1
+        else:
+           failed += 1
+        if sts == 400:
+           await db.delete_user(user['_id'])
+        done += 1
+        if not done % 20:
+           await sts_msg.edit(f"ʙʀᴏᴀᴅᴄᴀsᴛ ɪɴ ᴘʀᴏɢʀᴇss:\nᴛᴏᴛᴀʟ ᴜsᴇʀs: {total_users}\nᴛᴏᴛᴀʟ ʙʀᴏᴀᴅᴄᴀsᴛ: {done} / {total_users}\nsᴜᴄᴄᴇss: {success}\nғᴀɪʟᴇᴅ: {failed}")
+    completed_in = datetime.timedelta(seconds=int(time.time() - start_time))
+    await sts_msg.edit(f"ʙʀᴏᴀᴅᴄᴀsᴛ ᴄᴏᴍᴘʟᴇᴛᴇᴅ:\nᴄᴏᴍᴘʟᴇᴛᴇ ɪɴ `{completed_in}`.\n\nᴛᴏᴛᴀʟ ᴜsᴇʀs {total_users}\nᴛᴏᴛᴀʟ ʙʀᴏᴀᴅᴄᴀsᴛᴇᴅ: {done} / {total_users}\nsᴜᴄᴄᴇss: {success}\nғᴀɪʟᴇᴅ: {failed}")
+           
+async def send_msg(user_id, message):
     try:
-        await message.copy(chat_id=user_id)
-        return True, "Success"
+        await message.copy(chat_id=int(user_id))
+        return 200
     except FloodWait as e:
-        await asyncio.sleep(e.x)
-        return await broadcast_messages(user_id, message)
+        await asyncio.sleep(e.value)
+        return send_msg(user_id, message)
     except InputUserDeactivated:
-        await db.delete_user(int(user_id))
-        logging.info(f"{user_id} - Removed from database, since deleted account.")
-        return False, "Deleted"
+        logger.info(f"{user_id} : deactivated")
+        return 400
     except UserIsBlocked:
-        logging.info(f"{user_id} - Blocked the bot.")
-        return False, "Blocked"
+        logger.info(f"{user_id} : blocked the bot")
+        return 400
     except PeerIdInvalid:
-        await db.delete_user(int(user_id))
-        logging.info(f"{user_id} - PeerIdInvalid")
-        return False, "Error"
+        logger.info(f"{user_id} : user id invalid")
+        return 400
     except Exception as e:
-        return False, "Error"
-
-
-# Broadcast messages to all users
-
-@Hiroko.on_message(filters.command("broadcast") & filters.user(OWNER_ID) & filters.reply)
-async def broadcast_to_all(bot, message):
-    users = await get_served_users()
-    b_msg = message.reply_to_message
-    status = await message.reply_text(
-        text='Broadcasting your messages...'
-    )
-    start_time = time.time()
-    done = 0
-    blocked = 0
-    deleted = 0
-    failed = 0
-    success = 0
-    
-    async for user in users:
-        success, reason = await broadcast_messages(int(user['id']), b_msg)
-        if success:
-            success += 1
-        elif success is False:
-            if reason == "Blocked":
-                blocked += 1
-            elif reason == "Deleted":
-                deleted += 1
-            elif reason == "Error":
-                failed += 1
-        done += 1
-        
-        if not done % 20:
-            await status.edit(f"Broadcast in progress:\n\nTotal Users: {len(users)}\nCompleted: {done}/{len(users)}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")
-    
-    time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
-    await status.edit(f"Broadcast completed:\n\nTotal Users: {len(users)}\nCompleted: {done}/{len(users)}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}\n\nTime taken: {time_taken}")
-
-
-# Broadcast messages to all groups
-
-@Hiroko.on_message(filters.command("groupcast") & filters.user(OWNER_ID) & filters.reply)
-async def group_cast(bot, message):
-    chats = await get_served_chats()
-    b_msg = message.reply_to_message
-    status = await message.reply_text(
-        text='Broadcasting your messages...'
-    )
-    start_time = time.time()
-    done = 0
-    failed = 0
-    success = 0
-    
-    async for chat in chats:
-        success, reason = await broadcast_messages(int(chat['id']), b_msg)
-        if success:
-            success += 1
-        elif success is False:
-            if reason == "Error":
-                failed += 1
-        done += 1
-        await asyncio.sleep(2)
-        if not done % 20:
-            await status.edit(f"Broadcast in progress:\n\nTotal Chats: {len(chats)}\nCompleted: {done}/{len(chats)}\nSuccess: {success}\nFailed: {failed}")
-    
-    time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
-    await status.edit(f"Broadcast completed:\n\nTotal Chats: {len(chats)}\nCompleted: {done}/{len(chats)}\nSuccess: {success}\nFailed: {failed}\n\nTime taken: {time_taken}")
-
-
-
-
-
-
+        logger.error(f"{user_id} : {e}")
+        return 500
+ 
